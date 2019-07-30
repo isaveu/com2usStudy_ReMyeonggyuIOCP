aaabbb0a2a8e3e3178c24810012b6fc4be27cd06
Gnues <gnueskob@gmail.com>
2019-07-30 15:20:40 +0900

    Rename console host

---
+++ ChattingServer/Console/echoServer.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <thread>
+
+#include "../Lib_Logic/LogicMain.h"
+
+int main()
+{
+	constexpr int threadNumber = 2;
+	constexpr int sessionNumber = 1000;
+	constexpr int bufferSize = 1024;
+
+	const char* const ip = "127.0.0.1";
+	const short port = 23452;
+	const std::string name = "SampleServer";
+
+	const short maxUserNum = 800;
+	const short maxRoomNum = 100;
+
+	lsbLogic::LogicConfig config =
+	{
+		threadNumber,
+		sessionNumber,
+		bufferSize,
+		ip, port, name,
+		maxUserNum,
+		maxRoomNum,
+	};
+
+	lsbLogic::LogicMain myServer;
+	myServer.Init(config);
+	myServer.Start();
+
+	std::thread logicThread([&]()
+		{
+			myServer.Run();
+		}
+	);
+
+	// TODO: key 입력 시 종료
+	char a;
+	std::cin >> a;
+	myServer.Stop();
+	logicThread.join();
+
+	return 0;
+}
--- protoStruct/protoc.bat
+++ protoStruct/protoc.bat
@@ -1,5 +1,5 @@
-set CPP_DST_DIR=%cd%/../lsbLogic/Protobuf
-set CS_DST_DIR=%cd%/../echoClient_csharp/Protobuf
+set CPP_DST_DIR=%cd%/../ChattingServer/Lib_Logic/Protobuf
+set CS_DST_DIR=%cd%/../ChattingClient/Protobuf
 set SRC_DIR=%cd%
 protoc.exe -I=%SRC_DIR% --cpp_out=%CPP_DST_DIR% %SRC_DIR%/Packet.proto
 rem [Deprecated] protoc.exe -I=%SRC_DIR% --cpp_out=%CPP_DST_DIR% %SRC_DIR%/Define.proto
